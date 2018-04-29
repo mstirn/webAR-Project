7cbdf4fd630a0e0f2634bfeec795171c727177d4
Matthew Stirnweiss <mpstirn@outlook.com>
2018-04-29 01:09:54 -0400

    Working prototype with plane model, ability to change item location

--- client/index.js
+++ client/index.js
@@ -11,10 +11,6 @@ ReactDOM.render(
   <Provider store={store}>
     <div id='demo' className='fill-xy bg-yellow black column center-xy'>
       <div>
-        <div>Express&</div>
-        <div>Sequelize&</div>
-        <div>React&</div>
-        <div>Redux.</div>
       </div>
     </div>
   </Provider>,
